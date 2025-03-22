b9f3e2373b07c921f53b1d53661289061f7b7133
Faithy4444 <faithy4444@gmail.com>
2025-03-22 21:13:10 +0200

    level 100

--- script.js
+++ script.js
@@ -6,7 +6,19 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  
+  for(let episode of episodeList){
+    let elementContents = document.createElement("div")
+    elementContents.classList.add("episode")
+
+
+    elementContents.innerHTML = `<h4>${episode.name}-${episode.season}${episode.number}</h4>
+    <img  src ="${episode.image.medium}" alt = "${episode.name}">
+    <p>${episode.summary}</p>`
+    rootElem.appendChild(elementContents) 
+  }
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
 window.onload = setup;
