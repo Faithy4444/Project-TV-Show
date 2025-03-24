a7e1222272a22e30b889f8f3e3fd3089f71bb4fe
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-03-24 19:53:42 +0200

    Created variable for a search input and appended it to the search div

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 //You can edit ALL of the code here
+const rootElem = document.getElementById("root");
+
+const searchArea = document.getElementById("search");
+
+const searchInput = document.createElement("input");
+searchInput.type = "search";
+searchInput.placeholder = "Search here .....";
+
+searchArea.appendChild(searchInput);
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
+  //const rootElem = document.getElementById("root");
 
-  
-  for(let episode of episodeList){
-    let elementContents = document.createElement("div")
-    elementContents.classList.add("episode")
-
+  for (let episode of episodeList) {
+    let elementContents = document.createElement("div");
+    elementContents.classList.add("episode");
 
     elementContents.innerHTML = `<h4>${episode.name}-${episode.season}${episode.number}</h4>
     <img  src ="${episode.image.medium}" alt = "${episode.name}">
-    <p>${episode.summary}</p>`
-    rootElem.appendChild(elementContents) 
+    <p>${episode.summary}</p>`;
+    rootElem.appendChild(elementContents);
   }
   //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
